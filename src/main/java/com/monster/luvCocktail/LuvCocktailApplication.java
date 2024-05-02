39cc904834331212ef1904e8ddd73441a2a96e3f
TYKIM <xodus4194@gmail.com>
2024-05-02 10:07:41 +0900

    #1 first commit

---
+++ src/main/java/com/monster/luvCocktail/LuvCocktailApplication.java
@@ -0,0 +1,13 @@
+package com.monster.luvCocktail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuvCocktailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuvCocktailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/monster/luvCocktail/LuvCocktailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monster.luvCocktail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuvCocktailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
